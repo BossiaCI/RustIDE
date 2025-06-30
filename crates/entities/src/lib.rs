70422c55381e29f4ee8777b48c6b81f101c5496b
BossiaCI <bossiadaha@gmail.com>
2025-06-30 14:39:12 +0200

    initial repository commit

---
+++ crates/entities/src/lib.rs
@@ -0,0 +1,6 @@
+// Import necessary crates and modules.
+// parking_lot::Mutex is used for efficient and ergonomic thraed-safe mutable access.
+use parking_lot::Mutex;
+
+// ropey::Rope is a performant text buffer data structure, suitable for editors.
+use ropey::Rope;
